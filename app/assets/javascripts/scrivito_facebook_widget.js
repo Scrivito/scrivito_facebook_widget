653e52a2825002044af7a53dc29fcd80a2c4d88b
Gert Geidel <gert.geidel@infopark.de>
2016-06-01 17:10:19 +0200

    Update api

--- app/assets/javascripts/scrivito_facebook_widget.js
+++ app/assets/javascripts/scrivito_facebook_widget.js
@@ -1,14 +1,21 @@
 $(function() {
-  $('body').prepend('<div id="fb-root"></div>');
-
-  $.getScript('//connect.facebook.net/en_US/sdk.js', function(){
+  window.fbAsyncInit = function() {
     FB.init({
-      appId: ("fbAppId" in window) ? window.fbAppId : '',
-      version: 'v2.3'
+      appId      : ("fbAppId" in window) ? window.fbAppId : '',,
+      xfbml      : true,
+      version    : 'v2.5'
     });
+  };
 
-    scrivito.on('content', function() {
-      FB.XFBML.parse();
-    });
+  (function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+
+  scrivito.on('content', function() {
+    FB.XFBML.parse();
   });
 });
